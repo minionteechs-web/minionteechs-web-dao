afb64f3b21e698a4a921b3b2e41ff1044d9cd6a6
minionteechs-web <minionteechs@gmail.com>
2025-11-18 13:19:55 +0000

    A production-ready Decentralized Autonomous Organization (DAO) framework built with Solidity and Foundry.

--- src/GovernanceToken.sol
+++ src/GovernanceToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title GovernanceToken
