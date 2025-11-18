e7ef3bffeee923163c442b7004ac2380e3b67d8c
minionteechs-web <minionteechs@gmail.com>
2025-11-18 13:06:27 +0000

    A production-ready Decentralized Autonomous Organization (DAO) framework built with Solidity and Foundry.
    build error corrected

--- src/Governance.sol
+++ src/Governance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./IGovernanceToken.sol";
+import "./IGovernance.sol";
 
 /**
  * @title Governance
--- test/Governance.t.sol
+++ test/Governance.t.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/Governance.sol";
+import "../src/GovernanceToken.sol";
 
 contract GovernanceTest is Test {
     Governance governance;
+    GovernanceToken token;
 
     function setUp() public {
-        governance = new Governance();
+        token = new GovernanceToken();
+        governance = new Governance(address(token));
     }
 
     function testExample() public {
